bce45f83669bb720a62e4970008ce6f027db29b9
jdid <lamyaejdid20@gmail.com>
2021-10-15 21:36:42 +0100

    essaie8

--- src/Horloge.java
+++ src/Horloge.java
@@ -1,13 +1,13 @@
 public class Horloge {
-    int sec, min;
+    int sec, min,h;
 
     public Horloge(int sec, int min, int h) {
         this.sec = sec;
         this.min = min;
-        //this.h = h;
+        this.h = h;
     }
     public static void main(String[] argc){
         Horloge h = new Horloge(4,5,6);
-        System.out.println(h.sec+"\t"+h.min+"\t");
+        System.out.println(h.sec+"\t");
     }
 }

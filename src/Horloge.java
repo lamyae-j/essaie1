a879c8b29dd93df6fe263a7665c3f4c4970994b0
jdid <lamyaejdid20@gmail.com>
2021-10-15 21:58:50 +0100

    final

--- src/Horloge.java
+++ src/Horloge.java
@@ -6,6 +6,31 @@ public class Horloge {
         this.min = min;
         this.h = h;
     }
+
+    public int getSec() {
+        return sec;
+    }
+
+    public void setSec(int sec) {
+        this.sec = sec;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
     public static void main(String[] argc){
         Horloge h = new Horloge(4,5,6);
         System.out.println(h.sec+"\t");
